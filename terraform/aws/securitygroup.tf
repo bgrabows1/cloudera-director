0041ae30bd57680af95bdaa80f0da106296261d0
bgrabows1 <bgrabowski.bartek@gmail.com>
2019-03-06 17:21:18 +0000

    Fixes in deployment object and added ansible destroy playbook.

--- terraform/aws/securitygroup.tf
+++ terraform/aws/securitygroup.tf
@@ -27,7 +27,21 @@ resource "aws_security_group" "public" {
     from_port   = 7189
     to_port     = 7189
     protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags {
